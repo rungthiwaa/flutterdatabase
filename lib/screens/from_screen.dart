37f0e4897e60e1b033bcbf2e265d5378c33b33b9
rungthiwaa <ryawici@gmail.com>
2022-08-31 03:11:36 +0700

    ตรวจสอบ

--- lib/screens/from_screen.dart
+++ lib/screens/from_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Fromscreen extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,26 +11,45 @@ class Fromscreen extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Form(
+              key: formKey,
               child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: new InputDecoration(labelText: "ชื่อรายการ"),
-                autofocus: true,
-              ),
-              TextFormField(
-                decoration: new InputDecoration(labelText: "จำนวนเงิน"),
-                keyboardType: TextInputType.number,
-              ),
-              FlatButton(
-                  child: Text("เพิ่มข้อมูล"),
-                  color: Colors.red.shade400,
-                  textColor: Colors.white,
-                  onPressed: () {
-                    Navigator.pop(context);
-                  })
-            ],
-          )),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    decoration: new InputDecoration(labelText: "ชื่อรายการ"),
+                    autofocus: true,
+                    validator: (String? str) {
+                      if (str!.isEmpty) {
+                        return "กรุณาป้อนชื่อรายการ";
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+                    keyboardType: TextInputType.number,
+                    validator: (String? str) {
+                      if (str!.isEmpty) {
+                        return "กรุณาป้อนจำนวนเงิน";
+                      }
+
+                      if (double.parse(str) <= 0) {
+                        return "กรุณาป้อนตัวเลขมากกว่า0";
+                      }
+                      return null;
+                    },
+                  ),
+                  FlatButton(
+                      child: Text("เพิ่มข้อมูล"),
+                      color: Colors.red.shade400,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          Navigator.pop(context);
+                        }
+                      })
+                ],
+              )),
         ) // This trailing comma makes auto-formatting nicer for build methods.
         );
   }
